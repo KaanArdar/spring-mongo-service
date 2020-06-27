6a2affff0da9611ff0bcf3abec484d97830d0970
kaanardar <kaan.ardar@betbull.com>
2020-06-27 15:33:22 +0300

    added all controller layer

--- src/main/java/com/bilyoner/challenge/repository/DocumentDataRepository.java
+++ src/main/java/com/bilyoner/challenge/repository/DocumentDataRepository.java
@@ -1,13 +1,28 @@
 package com.bilyoner.challenge.repository;
 
+
 import com.bilyoner.challenge.model.entity.DocumentData;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DocumentDataRepository extends MongoRepository<DocumentData,Long> {
 
 
     Optional<DocumentData> findByNumber(Long number);
+
+    Optional<DocumentData> findFirstByOrderByNumberDesc();
+
+    Optional<DocumentData> findFirstByOrderByNumberAsc();
+
+    void deleteByNumber(Long number);
+
+    List<DocumentData> findAllByOrderByNumber(Sort sort);
+
+    List<DocumentData> findAllByOrderByNumberDesc();
+
+    List<DocumentData> findAllByOrderByNumberAsc();
 
 }
--- src/main/java/com/bilyoner/challenge/service/DocumentDateService.java
+++ src/main/java/com/bilyoner/challenge/service/DocumentDateService.java
@@ -2,12 +2,21 @@ package com.bilyoner.challenge.service;
 
 import com.bilyoner.challenge.model.dto.DocumentDataRequest;
 import com.bilyoner.challenge.model.dto.DocumentDataResponse;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 public interface DocumentDateService {
 
     DocumentDataResponse generateDocumentData(DocumentDataRequest request);
 
-    DocumentDataResponse getDocumentDataWithNumber(Long number);
+    DocumentDataResponse getDocumentDataWithNumber(long number);
 
+    DocumentDataResponse getDocumentDataForMaxNumber();
 
+    DocumentDataResponse getDocumentDataForMinNumber();
+
+    void deleteDocumentData(long number);
+
+    List<DocumentDataResponse> findAllDocumentDataWithOrder(Sort.Direction filter);
 }
